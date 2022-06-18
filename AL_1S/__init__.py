629ca3d517bcde5de84f54689228bd03657b961b
羽山Miki <Vikala.Miki@gmail.com>
2022-06-19 01:10:44 +0800

    重构了AL-1S的基本框架, 配置文件由env改为yml

---
+++ AL_1S/__init__.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter
+
+from AL_1S.config import InlineGoCQHTTP, RUNTIME_CONFIG
+
+
+def asgi():
+    return nonebot.get_asgi()
+
+
+def driver():
+    return nonebot.get_driver()
+
+
+def init():
+    nonebot.init(**RUNTIME_CONFIG)
+    driver().register_adapter(Adapter)
+    nonebot.load_plugins("AL_1S/plugins")
+    if InlineGoCQHTTP.enabled:
+        nonebot.load_plugin("nonebot_plugin_gocqhttp")
+    # init_database()
+    sleep(3)
+
+
+def run():
+    nonebot.run()
--- bot.py
+++ bot.py
@@ -1,40 +1,9 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from time import sleep
+# -*- coding:utf-8 -*-
+import AL_1S
 
-import nonebot
-from nonebot.adapters.onebot.v12 import Adapter as ONEBOT_V12Adapter
-
-# Custom your logger
-#
-# from nonebot.log import logger, default_format
-# logger.add("error.log",
-#            rotation="00:00",
-#            diagnose=False,
-#            level="ERROR",
-#            format=default_format)
-
-# You can pass some keyword args config to init function
-nonebot.init()
-app = nonebot.get_asgi()
-
-driver = nonebot.get_driver()
-driver.register_adapter(ONEBOT_V12Adapter)
-
-nonebot.load_builtin_plugins("echo")
-# nonebot.load_plugin("nonebot_plugin_gocqhttp")
-sleep(3)
-
-# Please DO NOT modify this file unless you know what you are doing!
-# As an alternative, you should use command `nb` or modify `pyproject.toml` to load plugins
-nonebot.load_from_toml("pyproject.toml")
-
-# Modify some config / config depends on loaded configs
-#
-# config = driver.config
-# do something...
-
+AL_1S.init()
+app = AL_1S.asgi()
 
 if __name__ == "__main__":
-    nonebot.logger.warning("Always use `nb run` to start the bot instead of manually running!")
-    nonebot.run(app="__mp_main__:app")
+    AL_1S.run()
